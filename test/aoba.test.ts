a0abf9dfaaf5b0532697bafa12c9dab99cb3ed54
yukihiratype2 <yukihiratype2@live.com>
2021-04-16 13:24:10 +0800

    fix test

--- test/aoba.test.ts
+++ test/aoba.test.ts
@@ -4,9 +4,11 @@ describe("Test", function () {
   it("window should not be undefined", async function () {
     expect(window).toBeTruthy()
   })
+
   it("should load Aoba Client", async function () {
     expect(new AobaClient({ server: ''})).toBeTruthy()
   })
+
   it("should call store.save", function () {
     const saveHandler = jest.fn()
     const aoba = new AobaClient({
@@ -15,10 +17,11 @@ describe("Test", function () {
         save: saveHandler
       }
     })
-    aoba.info('kksk')
+    aoba.info('abc', [1, 2, 3], { a: 'b' })
     expect(saveHandler.mock.calls.length).toBe(1)
-    expect(saveHandler.mock.calls[0][0]).toBeTruthy()
+    expect(saveHandler.mock.calls[0][0].message).toStrictEqual(['abc', [1, 2, 3], { a: 'b' }])
   })
+
   it("shoud call console.log", function () {
     window.console.log = jest.fn()
     const message = 'kksk'
